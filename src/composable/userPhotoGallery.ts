68862a76a2bbe61a4de48b79d2606edccb8efc33
Enzo <enzoluiz.brt@gmail.com>
2022-11-08 16:37:12 -0300

    add img on indexedDB

--- src/composable/userPhotoGallery.ts
+++ src/composable/userPhotoGallery.ts
@@ -4,9 +4,10 @@ import {
   CameraResultType,
   CameraSource,
   Photo,
+  
 } from "@capacitor/camera";
-//import { Preferences } from "@capacitor/preferences";
-//import { Filesystem, Directory } from "@capacitor/filesystem";
+import { Filesystem, Directory } from "@capacitor/filesystem";
+import { Preferences } from "@capacitor/preferences";
 
 export interface UserPhoto {
   filepath: string;
@@ -14,17 +15,44 @@ export interface UserPhoto {
 }
 
 export function userPhotoGallery() {
+  const PHOTO_STORAGE = "photos";
   const photos = ref<UserPhoto[]>([]);
 
-const convertBlobToBase64 = (blob: Blob) => 
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onerror = reject;
-    reader.onload = () => {
-      resolve(reader.result);
+  const cachePhotos = () => {
+    Preferences.set({
+      key: PHOTO_STORAGE,
+      value: JSON.stringify(photos.value), //JSO,parse()
+    });
+  };
+
+  const convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
+    // let base64Data: string;
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    const base64Data = (await convertBlobToBase64(blob)) as string;
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
     };
-    reader.readAsDataURL(blob);
-  })
+  };
 
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
@@ -32,15 +60,13 @@ const convertBlobToBase64 = (blob: Blob) =>
       source: CameraSource.Camera,
       quality: 80,
     });
-
-    const fileName = new Date().getTime() + '.jpeg';
-    const savedFileImage = {
-        filepath: fileName,
-        webviewPath: photo.webPath,
-    };
+    const fileName = new Date().getTime() + ".jpeg";
+    const savedFileImage = await savePicture(photo, fileName);
 
     photos.value = [savedFileImage, ...photos.value];
   };
+
+  watch(photos, cachePhotos);
 
   return {
     photos,
