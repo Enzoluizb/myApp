f92e61704bb59925f20757ac45e12e78ce2606af
Enzo <enzoluiz.brt@gmail.com>
2022-11-29 16:44:48 -0300

    correção de portugues

--- src/composable/userPhotoGallery.ts
+++ src/composable/userPhotoGallery.ts
@@ -1,15 +1,9 @@
-import { ref, onMounted, watch } from "vue";
-import {
-  Camera,
-  CameraResultType,
-  CameraSource,
-  Photo,
-} from "@capacitor/camera";
-import { Filesystem, Directory } from "@capacitor/filesystem";
-import { Preferences } from "@capacitor/preferences";
-import { isPlatform } from "@ionic/vue";
-import { Capacitor } from "@capacitor/core";
-
+import { ref, onMounted, watch } from 'vue';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Preferences } from '@capacitor/preferences';
+import { Capacitor } from '@capacitor/core';
+import { isPlatform } from '@ionic/vue';
 
 export interface UserPhoto {
   filepath: string;
@@ -17,13 +11,13 @@ export interface UserPhoto {
 }
 
 export function userPhotoGallery() {
-  const PHOTO_STORAGE = "photos";
+  const PHOTO_STORAGE = 'photos';
   const photos = ref<UserPhoto[]>([]);
 
   const cachePhotos = () => {
     Preferences.set({
       key: PHOTO_STORAGE,
-      value: JSON.stringify(photos.value), //JSO,parse()
+      value: JSON.stringify(photos.value), // JSON.parse(string) 
     });
   };
 
@@ -37,17 +31,16 @@ export function userPhotoGallery() {
       reader.readAsDataURL(blob);
     });
 
-  const savePicture = async (
-    photo: Photo,
-    fileName: string
-  ): Promise<UserPhoto> => {
+  const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
     let base64Data: string;
-    if (isPlatform("hybrid")) {
+    // "hybrid" will detect mobile - iOS or Android
+    if (isPlatform('hybrid')) {
       const file = await Filesystem.readFile({
         path: photo.path!,
       });
       base64Data = file.data;
     } else {
+      // Fetch the photo, read as a blob, then convert to base64 format
       const response = await fetch(photo.webPath!);
       const blob = await response.blob();
       base64Data = (await convertBlobToBase64(blob)) as string;
@@ -57,16 +50,19 @@ export function userPhotoGallery() {
       data: base64Data,
       directory: Directory.Data,
     });
-    if (isPlatform("hybrid")) {
+
+    if (isPlatform('hybrid')) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
       return {
         filepath: savedFile.uri,
-
         webviewPath: Capacitor.convertFileSrc(savedFile.uri),
       };
     } else {
+      // Use webPath to display the new image instead of base64 since it's
+      // already loaded into memory
       return {
         filepath: fileName,
-
         webviewPath: photo.webPath,
       };
     }
@@ -78,7 +74,7 @@ export function userPhotoGallery() {
       source: CameraSource.Camera,
       quality: 80,
     });
-    const fileName = new Date().getTime() + ".jpeg";
+    const fileName = new Date().getTime() + '.jpeg';
     const savedFileImage = await savePicture(photo, fileName);
 
     photos.value = [savedFileImage, ...photos.value];
@@ -86,27 +82,36 @@ export function userPhotoGallery() {
 
   const loadSaved = async () => {
     const photoList = await Preferences.get({ key: PHOTO_STORAGE });
-    const photosInPreferences = photoList.value
-      ? JSON.parse(photoList.value)
-      : [];
+    const photosInPreferences = photoList.value ? JSON.parse(photoList.value) : [];
 
-    if(!isPlatform('hybrid')) {
-    for (const photo of photosInPreferences) {
-      const file = await Filesystem.readFile({
-        path: photo.filepath,
-        directory: Directory.Data,
-      });
-      photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
-    }}
+    if (!isPlatform("hybrid")) {
+      for (const photo of photosInPreferences) {
+        const file = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data,
+        });
+        photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+      }
+    }
 
     photos.value = photosInPreferences;
   };
 
+
+  const deletePhoto = async (photo: UserPhoto) => {
+    photos.value = photos.value.filter((foto) => foto.filepath !== photo.filepath);
+    const filename = photo.filepath.substr(photo.filepath.lastIndexOf('/') + 1);
+    await Filesystem.deleteFile({
+      path: filename,
+      directory: Directory.Data,
+    });
+  };
 
   watch(photos, cachePhotos);
   onMounted(loadSaved);
   return {
     photos,
     takePhoto,
+    deletePhoto
   };
 }
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,69 +2,66 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Galeria de Fotos</ion-title>
+        <ion-title>Galeria de fotos</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+
       <ion-grid>
         <ion-row>
-          <ion-col size="5" :Key="photo" v-for="photo in photos">
-            <ion-img :src="photo.webviewPath"></ion-img>
+          <ion-col size="4" :Key="photo" v-for="photo in photos">
+            <ion-img @click="showActionSheet(photo)" :src="photo.webviewPath"></ion-img>
           </ion-col>
         </ion-row>
       </ion-grid>
 
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
         <ion-fab-button @click="takePhoto()">
-          <ion-icon :icon="camera" />
+          <ion-icon :icon="camera"></ion-icon>
         </ion-fab-button>
       </ion-fab>
     </ion-content>
   </ion-page>
 </template>
-
 <script lang="ts">
-import { defineComponent } from "vue";
-import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-  IonFab,
-  IonIcon,
-  IonFabButton,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonImg,
-} from "@ionic/vue";
-import { camera } from "ionicons/icons";
-import { userPhotoGallery } from "@/composable/userPhotoGallery";
+import { defineComponent } from 'vue';
+import { actionSheetController, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/vue';
+import { camera, trash, close } from "ionicons/icons";
+import { userPhotoGallery, UserPhoto } from "@/composable/userPhotoGallery";
 
 export default defineComponent({
-  name: "Tab1Page",
-  components: {
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonPage,
-    IonFab,
-    IonFabButton,
-    IonIcon,
-    IonGrid,
-    IonRow,
-    IonCol,
-    IonImg
-  },
+  name: 'Tab1Page',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg },
   setup() {
-    const { takePhoto, photos } = userPhotoGallery();
+    const { takePhoto, deletePhoto, photos } = userPhotoGallery();
+    const showActionSheet = async (photo: UserPhoto) => {
+      const actionSheet = await actionSheetController.create({
+        header: 'Photos',
+        buttons: [
+          {
+            text: 'Excluir',
+            role: 'destructive',
+            icon: trash,
+            handler: () => { deletePhoto(photo);},
+          },
+          {
+            text: 'Cancelar',
+            icon: close,
+            role: 'cancel',
+            handler: () => {/* Nenhuma ação quando cancelar*/}, 
+          },
+        ],
+      });
+      await actionSheet.present();
+    };
     return {
       camera,
+      trash,
+      close,
       takePhoto,
       photos,
-    };
+      showActionSheet
+    }
   },
 });
 </script>
